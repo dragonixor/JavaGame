ef273035aab13ec3064516522ed7a632194cd54a
HakimPay <hakimpayman@gmail.com>
2019-05-05 22:17:34 -0400

    HP- initial project commited

---
+++ src/src/mainGame/GameScreen.java
@@ -0,0 +1,136 @@
+package mainGame;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class GameScreen extends JPanel implements ActionListener, KeyListener{
+	/**
+	 * no clue what that is but it made a warning go away
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	/*-----Attributes for GameScreen-----*/
+	private final int SCREEN_WIDTH = Toolkit.getDefaultToolkit().getScreenSize().width;
+	private final int SCREEN_HEIGHT = Toolkit.getDefaultToolkit().getScreenSize().height;
+	private final int SCREEN_CENTER_X = SCREEN_WIDTH / 2;
+	private final int SCREEN_CENTER_Y = SCREEN_HEIGHT / 2;
+	private final int GAME_SCREEN_WIDTH = 800;
+	private final int GAME_SCREEN_HEIGHT = 600;
+	private final int GAME_SCREEN_X = SCREEN_CENTER_X - (GAME_SCREEN_WIDTH / 2);
+	private final int GAME_SCREEN_Y = SCREEN_CENTER_Y - (GAME_SCREEN_HEIGHT / 2);
+	private boolean gameIsOn = false;
+	private Timer timer;
+	private int delay = 0;
+	
+	/*-----Attributes for game entities-----*/
+	private int width = 100;
+	private int height = 100;
+	private int position_X = SCREEN_CENTER_X - (width / 2);
+	private int position_Y = SCREEN_CENTER_Y - (height / 2);
+	
+	//Constructor of GameScreen
+	public GameScreen() {
+		//Allows the current instance of GameScreen to receive inputs
+		addKeyListener(this);
+		
+		//The two following statements guarantees that the current instance of
+		//GameScreen will receive inputs and that the focus will not be taken 
+		//away from it
+		setFocusable(true);
+		setFocusTraversalKeysEnabled(false);
+		
+		timer = new Timer(delay, this);
+		timer.start();
+	}
+	
+	public void paint(Graphics g) {
+		//Clears the gameScreen
+		super.paint(g);
+		
+		//Background
+		g.setColor(Color.black);
+		g.fillRect(GAME_SCREEN_X, GAME_SCREEN_Y, GAME_SCREEN_WIDTH, GAME_SCREEN_HEIGHT);
+		
+		//Blue square
+		g.setColor(Color.blue);
+		g.fillRect(position_X, position_Y, width, height);
+		
+		
+	}
+	
+	public void moveLeft() {
+		gameIsOn = true;
+		if(position_X == (GAME_SCREEN_X)) {
+			position_X = GAME_SCREEN_X;
+		}else {
+			position_X -= 10;
+		}
+	}
+	
+	public void moveUp() {
+		gameIsOn = true;
+		if(position_Y == (GAME_SCREEN_Y)) {
+			position_Y = GAME_SCREEN_Y;
+		}else {
+			position_Y -= 10;
+		}
+	}
+	
+	public void moveRight() {
+		gameIsOn = true;
+		if(position_X == (GAME_SCREEN_X + GAME_SCREEN_WIDTH - width)) {
+			position_X = GAME_SCREEN_X + GAME_SCREEN_WIDTH - width;
+		}else {
+			position_X += 10;
+		}
+	}
+	
+	public void moveDown() {
+		gameIsOn = true;
+		if(position_Y == (GAME_SCREEN_Y + GAME_SCREEN_HEIGHT - height)) {
+			position_Y = GAME_SCREEN_Y + GAME_SCREEN_HEIGHT - height;
+		}else {
+			position_Y += 10;
+		}
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent ke) {
+		int keyCode = ke.getKeyCode();
+		
+		switch(keyCode) {
+		case KeyEvent.VK_LEFT:
+			moveLeft();
+			break;
+		case KeyEvent.VK_UP:
+			moveUp();
+			break;
+		case KeyEvent.VK_RIGHT:
+			moveRight();
+			break;
+		case KeyEvent.VK_DOWN:
+			moveDown();
+			break;
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent ke) {/*Not useful for now*/}
+
+	@Override
+	public void keyTyped(KeyEvent ke) {/*Not useful for now*/}
+
+	@Override
+	public void actionPerformed(ActionEvent ae) {
+		repaint();
+	}
+
+}
---
+++ src/src/mainGame/Main.java
@@ -0,0 +1,33 @@
+package mainGame;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame{
+	/*------Attributes------*/
+	private final int SCREEN_WIDTH = Toolkit.getDefaultToolkit().getScreenSize().width;
+	private final int SCREEN_HEIGHT = Toolkit.getDefaultToolkit().getScreenSize().height;
+	private final int SCREEN_CENTER_X = SCREEN_WIDTH / 2;
+	private final int SCREEN_CENTER_Y = SCREEN_HEIGHT / 2;
+	
+	/*------Main program------*/
+	public static void main(String[] args) {
+		Main m = new Main();
+	}
+	
+	/*------Functions used in the main program------*/
+	public Main() {
+		//Window setup
+		super("Shit Nuts");
+		setSize(SCREEN_WIDTH,SCREEN_HEIGHT);
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//Create the game screen and add it to the window
+		GameScreen gs = new GameScreen();
+		add(gs);
+		
+		//Makes sure everything within the window is visible
+		setVisible(true);
+	}
+}
